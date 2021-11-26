47b80c331795b9314d87fcd084ad9a151ca448a4
saranya-kethireddy <saranyakethireddy@gmail.com>
2021-11-26 16:19:36 +0530

    quiz app

---
+++ src/helpers/Questions.js
@@ -0,0 +1,26 @@
+export const Questions = [
+    {
+      prompt: "What is  DXC?",
+      optionA: "HOTEL",
+      optionB: "RESTAURANT",
+      optionC: "PARK",
+      optionD: "IT COMPANY",
+      asnwer: "optionD",
+    },
+    {
+      prompt: "Which of this is used for creating apps?",
+      optionA: "Teams",
+      optionB: "Oracle",
+      optionC: "VS code",
+      optionD: "Spring",
+      asnwer: "optionC",
+    },
+    {
+      prompt: "Which of this is a oops concept?",
+      optionA: "Method overriding",
+      optionB: "Constructor",
+      optionC: "Exception",
+      optionD: "Inheritance",
+      asnwer: "optionD",
+    },
+  ];
